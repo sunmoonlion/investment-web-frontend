ec8485f3dbb0e359797584166b32cbd9fcc79b47
sunmoon <13701819268@example.com>
2026-06-15 10:30:29 +0800

    chore: initialize investment-web-frontend

--- app/app/layout.tsx
+++ app/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: 'tpl App',
-  description: 'tpl web application',
+  title: 'investment App',
+  description: 'investment web application',
 }
 
 export default function RootLayout({
